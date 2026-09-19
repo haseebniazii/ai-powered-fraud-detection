ef462ce033516b7ca21fe5c8f888b766236e2a20
Haseeb Khan <mrkhanenterprisess@gmail.com>
2026-09-19 12:11:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // FastAPI Configuration
 // =========================================================
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://ai-powered-fraud-detection.fastapicloud.dev/";
 
 
 // =========================================================
